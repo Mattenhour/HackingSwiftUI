4b4d0325b84f7b5a1308795ef79ee84d2b3992e1
Matt Ridenhour <goofenhour@gmail.com>
2019-11-11 11:20:55 -0800

    Went back to day 25 and completed the milestone challenge for projects 1-3.

--- Milestone_Project_1-3/Milestone_Project_1-3/ContentView.swift
+++ Milestone_Project_1-3/Milestone_Project_1-3/ContentView.swift
@@ -12,13 +12,16 @@ struct ContentView: View {
     @State private var moves: [String] = ["Rock", "Paper", "Scissors"]
     @State private var appChoice: Int = 0
     @State private var playerScore: Int = 0
+    @State private var moveCount: Int = 0
     @State private var shouldWin: Bool = false
     
     
     var body: some View {
         VStack {
             Section {
-                Text("Players score: \(playerScore)")
+                if moveCount >= 10 {
+                    Text("Players score: \(playerScore)")
+                }
                 Text("Apps move: \(moves[appChoice])")
                 Text("Player should \(shouldWin ? "Win" : "Lose")")
             }
@@ -38,12 +41,71 @@ struct ContentView: View {
                 }
             }
         }
+        .onAppear(perform: StartGame)
+    }
+    
+    func StartGame() {
+        appChoice = Int.random(in: 0..<moves.count)
+        shouldWin = Bool.random()
     }
     
     func ButtonAction(_ playersMove: String) {
-        print("\(playersMove)")
-        playerScore += 1
-        //TODO: Add case statements logic for what to do
+//        print("\(playersMove)")
+        
+        let appsMove: String = moves[appChoice]
+        
+        if appsMove == playersMove {
+            playerScore -= 1
+            return
+        }
+        
+        shouldWin ? PlayerWin(playersMove) : PlayerLose(playersMove)
+        moveCount += 1
+        StartGame()
+    }
+    
+    // Logic for if player should win
+    func PlayerWin(_ playersMove: String){
+        
+        let appsMove: String = moves[appChoice]
+        
+        switch appsMove {
+        case "Rock":
+            if playersMove == "Paper" {
+                playerScore += 1
+            } else { playerScore -= 1 }
+        case "Paper":
+            if playersMove == "Scissors" {
+                playerScore += 1
+            } else { playerScore -= 1 }
+        default:
+            if playersMove == "Rock" {
+                playerScore += 1
+            } else { playerScore -= 1 }
+        }
+        
+    }
+    
+    // Logic for if player should lose
+    func PlayerLose(_ playersMove: String){
+        
+        let appsMove: String = moves[appChoice]
+        
+        switch appsMove {
+        case "Rock":
+            if playersMove == "Scissors" {
+                playerScore += 1
+            } else { playerScore -= 1 }
+        case "Paper":
+            if playersMove == "Rock" {
+                playerScore += 1
+            } else { playerScore -= 1 }
+        default:
+            if playersMove == "Paper" {
+                playerScore += 1
+            } else { playerScore -= 1 }
+        }
+        
     }
 }
 

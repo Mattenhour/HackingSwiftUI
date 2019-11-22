4aaa52cd9d469855aa87d8282fc9a1ff55c55a5d
Matt Ridenhour <goofenhour@gmail.com>
2019-11-21 18:39:17 -0800

    Lets see what this does

---
+++ Project7/Project7/Day36/ContentMain.swift
@@ -0,0 +1,21 @@
+//
+//  ContentMain.swift
+//  Project7
+//
+//  Created by Matt Ridenhour on 11/17/19.
+//  Copyright © 2019 Matt Ridenhour. All rights reserved.
+//
+
+import SwiftUI
+
+struct Day36Main: View {
+    var body: some View {
+        Text("Day 36")
+    }
+}
+
+struct Day36Main_Previews: PreviewProvider {
+    static var previews: some View {
+        Day36Main()
+    }
+}

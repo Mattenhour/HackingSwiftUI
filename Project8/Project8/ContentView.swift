08302de5cec975265ba77d8aee6c91422b800083
Matt Ridenhour <goofenhour@gmail.com>
2019-11-23 12:35:25 -0800

    Completed day 39. Started project 8.

---
+++ Project8/Project8/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project8
+//
+//  Created by Matt Ridenhour on 11/23/19.
+//  Copyright © 2019 Matt Ridenhour. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

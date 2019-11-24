de31599e8b2478b2c009446aefe9f9cb6e4bf88c
Matt Ridenhour <goofenhour@gmail.com>
2019-11-23 17:29:35 -0800

    Completed day 40.

---
+++ Project8/Project8/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Project8
+//
+//  Created by Matt Ridenhour on 11/23/19.
+//  Copyright © 2019 Matt Ridenhour. All rights reserved.
+//
+
+import Foundation
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}

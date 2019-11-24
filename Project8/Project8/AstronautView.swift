30457760c465f27b2e47bb43924c1e28e7611e1d
Matt Ridenhour <goofenhour@gmail.com>
2019-11-24 15:05:51 -0800

    Completed day 41.
    Will come back next time to change MissionView.swift images of each astronaut be displayed in a scrollView horizontal

---
+++ Project8/Project8/AstronautView.swift
@@ -0,0 +1,39 @@
+//
+//  AstronautView.swift
+//  Project8
+//
+//  Created by Matt Ridenhour on 11/24/19.
+//  Copyright © 2019 Matt Ridenhour. All rights reserved.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.astronaut.id)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width)
+
+                    Text(self.astronaut.description)
+                        .padding()
+                    .layoutPriority(1)
+                }
+            }
+        }
+        .navigationBarTitle(Text(astronaut.name), displayMode: .inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    static var previews: some View {
+        AstronautView(astronaut: astronauts[0])
+    }
+}

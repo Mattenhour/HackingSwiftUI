c73f28dcad5a9f5a884862825a13ec6beb746dac
Matt Ridenhour <goofenhour@gmail.com>
2020-02-12 21:05:21 -0800

    Ready for project 12 wrap up

---
+++ Project12/Singer+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Singer+CoreDataClass.swift
+//  Project12
+//
+//  Created by Matt Ridenhour on 2/11/20.
+//  Copyright © 2020 Matt Ridenhour. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
---
+++ Project12/Singer+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  Singer+CoreDataProperties.swift
+//  Project12
+//
+//  Created by Matt Ridenhour on 2/11/20.
+//  Copyright © 2020 Matt Ridenhour. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Singer {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Singer> {
+        return NSFetchRequest<Singer>(entityName: "Singer")
+    }
+
+    @NSManaged public var firstName: String?
+    @NSManaged public var lastName: String?
+    
+    var wrappedFirstName: String {
+        firstName ?? "Unknown"
+    }
+
+    var wrappedLastName: String {
+        lastName ?? "Unknown"
+    }
+
+}

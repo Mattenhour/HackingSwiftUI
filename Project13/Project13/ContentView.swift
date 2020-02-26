6f375a3aa3e8e6010df0510ffe1636df70fa119f
Matt Ridenhour <goofenhour@gmail.com>
2020-02-25 20:01:53 -0800

    Day 62 completed. Some lessons did not work while I was following the instructions. This is very frustrating

---
+++ Project13/Project13/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  Project13
+//
+//  Created by Matt Ridenhour on 2/25/20.
+//  Copyright © 2020 Matt Ridenhour. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingActionSheet = false
+    @State private var backgroundColor = Color.white
+    
+    var body: some View {
+        Text("Hello, World!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                self.showingActionSheet = true
+            }
+        .actionSheet(isPresented: $showingActionSheet) {
+            ActionSheet(title: Text("Change background"), message: Text("Select new color"), buttons: [
+                .default(Text("Red")) { self.backgroundColor = .red },
+                .default(Text("Green")) { self.backgroundColor = .green },
+                .default(Text("Blue")) { self.backgroundColor = .blue },
+                .cancel()
+            ])
+        }
+    }
+        
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

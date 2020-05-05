a866f1ac835b34d336b7ea8150e2e9771c4dc590
Matt Ridenhour <goofenhour@gmail.com>
2020-05-04 20:04:33 -0700

    Completed Day 86. Took last week off

---
+++ Project17/Project17/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project17
+//
+//  Created by Matt Ridenhour on 4/29/20.
+//  Copyright © 2020 Matt Ridenhour. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

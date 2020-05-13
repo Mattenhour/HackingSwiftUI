98b980adcb099f250cc5a75bed65f22b2f14c40c
Matt Ridenhour <goofenhour@gmail.com>
2020-05-13 16:18:34 -0700

    Completed day 90

--- Project17/Project17/CardView.swift
+++ Project17/Project17/CardView.swift
@@ -10,12 +10,16 @@ import SwiftUI
 
 struct CardView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityEnabled) var accessibilityEnabled
     
     let card: Card
     var removal: (() -> Void)? = nil
     
     @State private var isShowingAnswer = false
+    
     @State private var offset = CGSize.zero
+    
+    @State private var feedback = UINotificationFeedbackGenerator()
     
     var body: some View {
         ZStack {
@@ -26,53 +30,68 @@ struct CardView: View {
                         : Color.white
                             .opacity(1 - Double(abs(offset.width / 50)))
             )
-            .background(
-                differentiateWithoutColor
-                ? nil
-                : RoundedRectangle(cornerRadius: 25, style: .continuous)
-                    .fill(offset.width > 0 ? Color.green : Color.red)
+                .background(
+                    differentiateWithoutColor
+                        ? nil
+                        : RoundedRectangle(cornerRadius: 25, style: .continuous)
+                            .fill(offset.width > 0 ? Color.green : Color.red)
             )
-            .shadow(radius: 10)
+                .shadow(radius: 10)
             
             VStack {
-                Text(card.prompt)
-                    .font(.largeTitle)
-                    .foregroundColor(.black)
-                
-                if isShowingAnswer {
-                    Text(card.answer)
-                        .font(.title)
-                        .foregroundColor(.gray)
+                if accessibilityEnabled {
+                    Text(isShowingAnswer ? card.answer : card.prompt)
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                } else {
+                    Text(card.prompt)
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                    
+                    if isShowingAnswer {
+                        Text(card.answer)
+                            .font(.title)
+                            .foregroundColor(.gray)
+                    }
                 }
             }
             .padding(20)
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
-        
+            
         .rotationEffect(.degrees(Double(offset.width / 5)))
         .offset(x: offset.width * 5, y: 0)
         .opacity(2 - Double(abs(offset.width / 50)))
-        
-    .gesture(
-        DragGesture()
-            .onChanged { gesture in
-                self.offset = gesture.translation
+        .accessibility(addTraits: .isButton)
+            
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    self.offset = gesture.translation
+                    self.feedback.prepare()
             }
-        
-        .onEnded { _ in
-            if abs(self.offset.width) > 100 {
-                // remove the card
-                self.removal?() // ? means closure will only be called if it has been set
-            } else {
-                self.offset = .zero
+                
+            .onEnded { _ in
+                if abs(self.offset.width) > 100 {
+                    
+                    // Error hapitic when swiping left
+                    if self.offset.width < 0 {
+                        self.feedback.notificationOccurred(.error)
+                    }
+                    
+                    // remove the card
+                    self.removal?() // ? means closure will only be called if it has been set
+                    
+                } else {
+                    self.offset = .zero
+                }
             }
+        )
+            .onTapGesture {
+                self.isShowingAnswer.toggle()
         }
-    )
-        
-        .onTapGesture {
-            self.isShowingAnswer.toggle()
-        }
+        .animation(.spring())
     }
 }
 
--- Project17/Project17/ContentView.swift
+++ Project17/Project17/ContentView.swift
@@ -17,16 +17,22 @@ extension View {
 
 struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
-    @State private var cards = [Card](repeating: Card.example, count: 10)
+    @Environment(\.accessibilityEnabled) var accessibilityEnabled
+    
+    @State private var cards = [Card]()
     
     @State private var isActive = true
+    @State private var showingEditScreen = false
     
     @State private var timeRemaining = 100
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         ZStack {
-            Image("background")
+            /*
+             using decorative stops VocieOver from reading background image
+             */
+            Image(decorative: "background")
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
@@ -51,6 +57,8 @@ struct ContentView: View {
                             }
                         }
                         .stacked(at: index, total: self.cards.count)
+                        .allowsHitTesting(index == self.cards.count - 1)
+                        .accessibility(hidden: index < self.cards.count - 1) // VoiceOver stopped for all cards before last one
                     }
                 }
                 .allowsHitTesting(timeRemaining > 0)
@@ -64,28 +72,67 @@ struct ContentView: View {
                 }
             }
             
-            if differentiateWithoutColor {
+            VStack {
+                HStack {
+//                    Spacer()
+
+                    Button(action: {
+                        self.showingEditScreen = true
+                    }) {
+                        Image(systemName: "plus.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .clipShape(Circle())
+                    }
+                }
+
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .padding()
+            
+            if differentiateWithoutColor || accessibilityEnabled {
                 VStack {
                     Spacer()
                     
                     HStack {
-                        Image(systemName: "xmark.circle")
-                            .padding()
-                            .background(Color.black.opacity(0.7))
-                            .clipShape(Circle())
+                        // Marked wrong
+                        Button(action: {
+                            withAnimation {
+                                self.removeCard(at: self.cards.count - 1)
+                            }
+                        }) {
+                            Image(systemName: "xmark.circle")
+                                .padding()
+                                .background(Color.black.opacity(0.7))
+                                .clipShape(Circle())
+                        }
+                        .accessibility(label: Text("Wrong"))
+                        .accessibility(hint: Text("Mark your answer as being incorrect."))
                         
                         Spacer()
                         
-                        Image(systemName: "checkmark.circle")
-                            .padding()
-                            .background(Color.black.opacity(0.7))
-                            .clipShape(Circle())
+                        // Marked correct
+                        Button(action: {
+                            withAnimation {
+                                self.removeCard(at: self.cards.count - 1)
+                            }
+                        }) {
+                            Image(systemName: "checkmark.circle")
+                                .padding()
+                                .background(Color.black.opacity(0.7))
+                                .clipShape(Circle())
+                        }
+                        .accessibility(label: Text("Correct"))
+                        .accessibility(hint: Text("Mark your answer as being correct."))
                     }
                     .foregroundColor(.white)
                     .font(.largeTitle)
                     .padding()
                 }
             }
+            
         }
         .onReceive(timer) { time in
             guard self.isActive else { return }
@@ -104,9 +151,17 @@ struct ContentView: View {
                 self.isActive = true
             }
         }
+        
+        .sheet(isPresented: $showingEditScreen, onDismiss: resetCard) {
+            EditCards()
+        }
+        
+        .onAppear(perform: resetCard)
     }
     
     func removeCard(at index: Int) {
+        guard index >= 0 else { return }
+        
         cards.remove(at: index)
         
         if cards.isEmpty {
@@ -115,9 +170,17 @@ struct ContentView: View {
     }
     
     func resetCard() {
-        cards = [Card](repeating: Card.example, count: 10)
         timeRemaining = 100
         isActive = true
+        loadData()
+    }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                self.cards = decoded
+            }
+        }
     }
 }
 
